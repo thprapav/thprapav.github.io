c6d75eae498e89c5094d0cd8392663304c64f572
Theofanis prapavesis Semetzidis <thprapav@gmail.com>
2021-01-04 22:15:02 +0200

    feat: fix postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,23 +1,42 @@
-module.exports = {
-  plugins: [
-    'postcss-import',
-    'tailwindcss',
-    'postcss-flexbugs-fixes',
-    'postcss-100vh-fix',
-    'postcss-hover-media-feature',
-    [
-      'postcss-preset-env',
-      {
-        autoprefixer: {
-          flexbox: 'no-2009',
-        },
-        stage: 3,
-        features: {
-          'custom-properties': false,
-        },
+const commonPlugins = [
+  'postcss-import',
+  'tailwindcss',
+  'postcss-flexbugs-fixes',
+  'autoprefixer',
+  'postcss-100vh-fix',
+  'postcss-hover-media-feature',
+  [
+    'postcss-preset-env',
+    {
+      autoprefixer: {
+        flexbox: 'no-2009',
       },
-    ],
-    ,
-    'autoprefixer',
+      stage: 3,
+      features: {
+        'custom-properties': false,
+      },
+    },
   ],
-};
+];
+
+module.exports =
+  process.env.NODE_ENV !== 'development'
+    ? {
+        plugins: [
+          ...commonPlugins,
+          [
+            'cssnano',
+            {
+              preset: 'default',
+            },
+          ],
+          [
+            '@fullhuman/postcss-purgecss',
+            {
+              content: ['./**/*.tsx'],
+              defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+            },
+          ],
+        ],
+      }
+    : commonPlugins;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  important: true,
-  purge: {
-    content: ['./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}'],
-  },
+  purge: false,
   darkMode: 'class',
   theme: {
     extend: {
@@ -13,6 +10,8 @@ module.exports = {
       },
     },
   },
-  variants: {},
+  variants: {
+    extend: {},
+  },
   plugins: [require('@tailwindcss/typography'), require('@tailwindcss/aspect-ratio')],
 };
